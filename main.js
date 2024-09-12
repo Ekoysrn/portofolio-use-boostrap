1325b03a643600ce013b08402d21eb1e750a1ab3
Ekoysrn <ekoyusronardiansyah@gmail.com>
2024-09-12 11:31:55 +0700

    modified techsctack

--- main.js
+++ main.js
@@ -1,97 +1,126 @@
-/*!
- * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
- * Copyright 2011-2024 The Bootstrap Authors
- * Licensed under the Creative Commons Attribution 3.0 Unported License.
- */
-
 (() => {
-  'use strict'
+  "use strict";
 
-  const getStoredTheme = () => localStorage.getItem('theme')
-  const setStoredTheme = theme => localStorage.setItem('theme', theme)
+  const getStoredTheme = () => localStorage.getItem("theme");
+  const setStoredTheme = (theme) => localStorage.setItem("theme", theme);
 
   const getPreferredTheme = () => {
-    const storedTheme = getStoredTheme()
+    const storedTheme = getStoredTheme();
     if (storedTheme) {
-      return storedTheme
+      return storedTheme;
     }
 
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-  }
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  };
 
-  const setTheme = theme => {
-    if (theme === 'auto') {
-      document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+  const setTheme = (theme) => {
+    if (theme === "auto") {
+      document.documentElement.setAttribute(
+        "data-bs-theme",
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+          ? "dark"
+          : "light"
+      );
     } else {
-      document.documentElement.setAttribute('data-bs-theme', theme)
+      document.documentElement.setAttribute("data-bs-theme", theme);
     }
-  }
+    updateImageTheme(theme); //memperbarui image
+  };
 
-  setTheme(getPreferredTheme())
+  setTheme(getPreferredTheme());
 
   const showActiveTheme = (theme, focus = false) => {
-    const themeSwitcher = document.querySelector('#bd-theme')
+    const themeSwitcher = document.querySelector("#bd-theme");
 
     if (!themeSwitcher) {
-      return
+      return;
     }
 
-    const themeSwitcherText = document.querySelector('#bd-theme-text')
-    const activeThemeIcon = document.querySelector('.theme-icon-active use')
-    const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-    const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
+    const themeSwitcherText = document.querySelector("#bd-theme-text");
+    const activeThemeIcon = document.querySelector(".theme-icon-active use");
+    const btnToActive = document.querySelector(
+      `[data-bs-theme-value="${theme}"]`
+    );
+    const svgOfActiveBtn = btnToActive
+      .querySelector("svg use")
+      .getAttribute("href");
 
-    document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-      element.classList.remove('active')
-      element.setAttribute('aria-pressed', 'false')
-    })
+    document.querySelectorAll("[data-bs-theme-value]").forEach((element) => {
+      element.classList.remove("active");
+      element.setAttribute("aria-pressed", "false");
+    });
 
-    btnToActive.classList.add('active')
-    btnToActive.setAttribute('aria-pressed', 'true')
-    activeThemeIcon.setAttribute('href', svgOfActiveBtn)
-    const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
-    themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
+    btnToActive.classList.add("active");
+    btnToActive.setAttribute("aria-pressed", "true");
+    activeThemeIcon.setAttribute("href", svgOfActiveBtn);
+    const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
+    themeSwitcher.setAttribute("aria-label", themeSwitcherLabel);
 
     if (focus) {
-      themeSwitcher.focus()
+      themeSwitcher.focus();
+    }
+  };
+
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", () => {
+      const storedTheme = getStoredTheme();
+      if (storedTheme !== "light" && storedTheme !== "dark") {
+        setTheme(getPreferredTheme());
+      }
+    });
+
+  window.addEventListener("DOMContentLoaded", () => {
+    showActiveTheme(getPreferredTheme());
+
+    document.querySelectorAll("[data-bs-theme-value]").forEach((toggle) => {
+      toggle.addEventListener("click", () => {
+        const theme = toggle.getAttribute("data-bs-theme-value");
+        setStoredTheme(theme);
+        setTheme(theme);
+        showActiveTheme(theme, true);
+      });
+    });
+  });
+})();
+
+// function to change icon theme techstack
+function updateImageTheme(selectedTheme) {
+  // get all element image
+  const images = document.querySelectorAll(".techs");
+  const most = document.querySelector(".most");
+
+  // mendeteksi apakah tema gelap diaktifkan
+  const theme =
+    selectedTheme === "auto"
+      ? window.matchMedia("(prefers-color-scheme : dark)").matches
+        ? "dark"
+        : "light"
+      : selectedTheme;
+
+  if (most) {
+    if (theme === "light") {
+      most.src = most.src.replace(/&theme=dark/, "");
     }
   }
 
-  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-    const storedTheme = getStoredTheme()
-    if (storedTheme !== 'light' && storedTheme !== 'dark') {
-      setTheme(getPreferredTheme())
+  images.forEach((img) => {
+    if (img.src.includes("?theme=")) {
+      img.src = img.src.replace(/(theme=)(light|dark)/, `$1${theme}`);
     }
-  })
+  });
+}
 
-  window.addEventListener('DOMContentLoaded', () => {
-    showActiveTheme(getPreferredTheme())
 
-    document.querySelectorAll('[data-bs-theme-value]')
-      .forEach(toggle => {
-        toggle.addEventListener('click', () => {
-          const theme = toggle.getAttribute('data-bs-theme-value')
-          setStoredTheme(theme)
-          setTheme(theme)
-          showActiveTheme(theme, true)
-        })
-      })
-  })
-})()
+document.addEventListener("DOMContentLoaded", () =>
+  updateImageTheme(getPreferredTheme())
+);
 
-// function updateIcon(theme) {
-//     const themeIcon = document.querySelector('.theme-icon-active');
-//     if (theme === 'dark') {
-//         themeIcon.classList.remove('bi-sun-fill');
-//         themeIcon.classList.add('bi-moon-stars');
-//     } else if (theme === 'light') {
-//         themeIcon.classList.remove('bi-moon-stars');
-//         themeIcon.classList.add('bi-sun-fill');
-//     } else {
-//         themeIcon.classList.remove('bi-sun-fill');
-//         themeIcon.classList.add('bi-circle-half');
-//     }
-// }
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", () => updateImageTheme(getPreferredTheme()));
 
 // setInterval
 
@@ -102,15 +131,11 @@ function myTime() {
   document.getElementById("time").innerHTML = d.toLocaleTimeString();
 }
 
-
 // Array of texts to display with auto writing effect
-const texts = [
-  "Software Engineer",
-  "Web Developer"
-];
+const texts = ["Software Engineer", "Web Developer"];
 
 // DOM element to display the text
-const targetElement = document.getElementById('type');
+const targetElement = document.getElementById("type");
 
 let currentTextIndex = 0;
 let charIndex = 0;
@@ -119,7 +144,7 @@ const pauseBetweenTexts = 2000; // Pause between texts
 
 // Function to clear the current text
 function clearText() {
-  targetElement.innerHTML = '';
+  targetElement.innerHTML = "";
 }
 
 // Function to simulate typing effect
